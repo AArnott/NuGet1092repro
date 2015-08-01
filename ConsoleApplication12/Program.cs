73de3e8be82786c82ad13135c907494a66a3d35b
Andrew Arnott <andarno@microsoft.com>
2015-08-01 09:28:27 -0700

    Add repro project

---
+++ ConsoleApplication12/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft;
+using Microsoft.VisualStudio.Threading;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Requires.NotNull(args, nameof(args));
+            JoinableTaskFactory jtf;
+        }
+    }
+}
